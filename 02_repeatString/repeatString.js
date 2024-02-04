b84e33ca5b8769014c99442b2abd23af9a8775ec
scott@cassidylabs.com <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-03 21:09:24 -0800

    Complete test conditions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+// let string = "hey"
+// let num = 3
 
+
+const repeatString = function(string,num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let myString = ""
+    for (let i = num; i > 0; i--){
+        myString = myString.concat(string)
+    }
+    return myString
 };
 
 // Do not edit below this line
