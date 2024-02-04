e98bf03ffcd019265c4d3e66035332ce1d63f902
scott@cassidylabs.com <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-03 21:47:07 -0800

    Complete tests for exercises 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let givenString = string.split("")
+    let reverseIt = givenString.reverse()
+    let joinIt = reverseIt.join("")
+    return joinIt
 };
 
 // Do not edit below this line
