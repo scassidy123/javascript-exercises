0619f32055a355009046d87b74c4ddd5d2bc9cc5
scott@cassidylabs.com <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-06 14:16:17 -0800

    Complete exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const arraySum = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0)
+    return arraySum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let accumulator = 1
+  for (let num of arr) {
+    accumulator = num * accumulator
+  }
+  return accumulator
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1
+
+	let result = num
+  while (num > 1) {
+    num--
+    result *= num
+  }
+  return result
 };
 
+// factorial(5)
+// multiply([1,2,5])
+// sum([1,2,3,4,5])
+// add(2,2)
 // Do not edit below this line
 module.exports = {
   add,
