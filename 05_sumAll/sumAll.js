edb5ee4088ccf9e76276dbd99909d3945790e75a
scott@cassidylabs.com <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-05 17:14:37 -0800

    Complete exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    array = []
+    if (num1 < 0 || num2 < 0) { return "ERROR"}
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) { return "ERROR" }
+    if (num1 > num2) {
+        let holdForASecond = num1
+        num1 = num2
+        num2 = holdForASecond
+    }
 
+        for (let i = num1; i <= num2; i++) {
+            array.push(i)    
+        }
+        
+    let sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum
 };
-
+sumAll(10,1)
 // Do not edit below this line
 module.exports = sumAll;
